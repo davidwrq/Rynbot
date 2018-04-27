a2bfafb9fed1b57d624236e750ef940ef31badd9
david <davidw.rq@gmail.com>
2018-04-26 21:18:46 -0300

    first commit

---
+++ utils.py
@@ -0,0 +1,12 @@
+import json
+
+with open('secrets.json') as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    '''Get the secret variable or return explicit exception.'''
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = 'Set the {0} environment variable'.format(setting)
+        raise ImproperlyConfigured(error_msg)
